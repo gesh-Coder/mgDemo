36126dd7a71256ea90c78f9c84871dd6f84786f0
sangeshwaran sambath <mailsangesh.gmail.com>
2019-08-14 21:03:52 +0530

    Updates

---
+++ precache-manifest.6ef599b779ca8d2ea78fd626820c0552.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "121344c85ba58c3b8f5e93f1354d45c7",
+    "url": "/mgDemo/index.html"
+  },
+  {
+    "revision": "78ca4a70440575c5c665",
+    "url": "/mgDemo/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "d67aa471fbd73d2376f9",
+    "url": "/mgDemo/static/css/main.5eb45c72.chunk.css"
+  },
+  {
+    "revision": "78ca4a70440575c5c665",
+    "url": "/mgDemo/static/js/2.932d3820.chunk.js"
+  },
+  {
+    "revision": "d67aa471fbd73d2376f9",
+    "url": "/mgDemo/static/js/main.dbfb8fa1.chunk.js"
+  },
+  {
+    "revision": "124ccb3ca6a9bc85b390",
+    "url": "/mgDemo/static/js/runtime~main.8f27c1d2.js"
+  },
+  {
+    "revision": "1cc2e638367e48e0caf947a00fb4b209",
+    "url": "/mgDemo/static/media/Nav_Search.1cc2e638.svg"
+  },
+  {
+    "revision": "1e6a6bb47b29a98a94266cce6e47e53b",
+    "url": "/mgDemo/static/media/img_01.1e6a6bb4.jpg"
+  },
+  {
+    "revision": "bb10774732e72aaf9b8b4b9c4ca28723",
+    "url": "/mgDemo/static/media/logo.bb107747.png"
+  },
+  {
+    "revision": "e0f84e82128d9860370dd763acaed4a8",
+    "url": "/mgDemo/static/media/main_slider_01.e0f84e82.jpg"
+  },
+  {
+    "revision": "53f03e9a262ebd9882c911dcb809f2a2",
+    "url": "/mgDemo/static/media/main_slider_02.53f03e9a.jpg"
+  },
+  {
+    "revision": "1a99d936c8fce16311c4f8bdf19e9c56",
+    "url": "/mgDemo/static/media/menu_exp.1a99d936.svg"
+  },
+  {
+    "revision": "076ae806171a817415093d06ccab1819",
+    "url": "/mgDemo/static/media/mg_logo_Head.076ae806.png"
+  },
+  {
+    "revision": "fd46e4e0e5484b3c2e6f33785581063e",
+    "url": "/mgDemo/static/media/photo_add.fd46e4e0.JPG"
+  },
+  {
+    "revision": "ac743d8bb86080818843ba7ce96a277c",
+    "url": "/mgDemo/static/media/photo_img.ac743d8b.png"
+  },
+  {
+    "revision": "f5130c5e2271cf832d7764edc511dee0",
+    "url": "/mgDemo/static/media/youtube_logo.f5130c5e.png"
+  }
+]);
